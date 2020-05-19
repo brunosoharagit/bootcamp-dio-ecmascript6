d344be34228aa5b4768bbe9b3010ab7f2075d4a9
brunosoharagit <b.sohara.git@gmail.com>
2020-05-19 09:59:34 -0300

    update

--- Spreads_ECs_e_lacos/spreads.js
+++ Spreads_ECs_e_lacos/spreads.js
@@ -1,2 +1,16 @@
-var partes = ['Ombro', 'Braço']
-var corpo = ['Cabeça', ...partes, 'Pés']
+const user = 
+{
+    nome: 'Breno',
+    idade: '22',
+    endereco:
+    {
+        cidade: 'São José do Rio Preto',
+        uf: 'São Paulo',
+        pais: 'Brasil'
+    },
+}
+console.log(user)
+
+const user1 = {...user, endereco: {...user.endereco}, nome: 'Bruno', idade: 23}
+console.log(user1)
+
